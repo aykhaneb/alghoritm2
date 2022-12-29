c8809be0673323e8065bebe55a95236bfa6f6d2c
User <User@WIN-SUEO6K7NQMN>
2022-12-29 10:54:54 +0400

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //task no 1;
+
+            //int gun = int.Parse(Console.ReadLine());
+            //int il,ay,yas;
+            //il = gun / 12;
+            //ay = gun % 12;
+            //Console.WriteLine($"{il} il {ay} ay");
+
+            //task no2;
+
+            int n = int.Parse(Console.ReadLine());
+            int r, count=0;
+            while (n > 9)
+            {
+                r = n % 10;
+                n = n / 10;
+
+                if (r == 0)
+                {
+                    count++;
+                }
+                
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
